06249c94da080dda35a5ab42f1b540978bda00dd
Alex Campbell <axc1082@student.bham.ac.uk>
2020-08-21 13:55:22 +0100

    added a unit test to check that root goes to the post list, also removed the annoying warning about admin not being unique.

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import resolve
+from .views import post_list
 
 # Create your tests here.
+
+class HomePageTest(TestCase):
+
+	def test_root_goes_to_home_page(self):
+		found = resolve('/')
+		self.assertEqual(found.func, post_list)
